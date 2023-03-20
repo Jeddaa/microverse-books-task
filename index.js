6dac438d5ced4034c9c2fb5a39238b291a47dea3
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-03-20 13:18:50 +0300

    Modify content

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
+const books = [
+    {
+        title: 'First book',
+        author: 'Author 1'
+    }, {
+        title: 'Second book',
+        author: 'Author 2'
+    }
+]
+
+function bookList(books) {
+    books.forEach(detail => {
+        const book = document.createElement('div');
+        book.innerHTML = `
+        <p>${detail.title}</p>
+        <p>${detail.author}</p>
+        <button id ="close">Remove</button>
+        <br>
+        <hr>
+        `;
+        container.appendChild(book);
+    });
+}
+
+bookList(books);
